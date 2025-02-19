3747c524ab4b313458d9d4941be624b843c22156
SonYoungsung <sysgigigi@gmail.com>
2025-02-19 14:30:34 +0900

    publish: v0.0.3

--- packages/frontend/synthesizer/src/tokamak/resources/index.ts
+++ packages/frontend/synthesizer/src/tokamak/resources/index.ts
@@ -1,6 +1,11 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export * from './globalWireList.js'
 export * from './subcircuitInfo.js'
 
-export const wasmDir = './src/tokamak/constant/wasm'
+export const wasmDir = path.join(__dirname, '../constant/wasm');
 
