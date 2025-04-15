92864c8f7d7ccaa394cba536a07be82eb45d9122
SonYoungsung <sysgigigi@gmail.com>
2025-04-15 15:56:45 +0900

    chore(qap): create a gitignore

--- packages/frontend/qap-compiler/dist/setupParams.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const setupParams: {
-    l: number;
-    l_D: number;
-    m_D: number;
-    n: number;
-    s_D: number;
-};
--- packages/frontend/qap-compiler/dist/subcircuitInfo.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const subcircuits: {
-    id: number;
-    name: string;
-    Nwires: number;
-    Nconsts: number;
-    Out_idx: number[];
-    In_idx: number[];
-    flattenMap: number[];
-}[];
