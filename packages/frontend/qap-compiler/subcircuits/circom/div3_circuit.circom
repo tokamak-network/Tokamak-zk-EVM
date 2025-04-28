b20f8a6f68ef0bee7edbfefe337421793e7e6fbd
JehyukJang <cjhyuck213@gmail.com>
2025-04-28 19:17:06 +0800

    qap-compiler optimization WIP

--- packages/frontend/qap-compiler/subcircuits/circom/div3_circuit.circom
+++ packages/frontend/qap-compiler/subcircuits/circom/div3_circuit.circom
@@ -1,4 +1,4 @@
 pragma circom 2.1.6;
 include "../../components/div.circom";
 
-component main {public [in]} = Div();
+component main {public [in]} = Div3();
--- packages/frontend/qap-compiler/subcircuits/library/setupParams.ts
+++ packages/frontend/qap-compiler/subcircuits/library/setupParams.ts
@@ -6,12 +6,12 @@
   // s_D: The number of subcircuits in the library
   export const setupParams = 
  {
-  "l": 16,
-  "l_in": 11,
-  "l_out": 1,
-  "l_D": 80,
-  "m_D": 939,
+  "l": 1,
+  "l_in": 0,
+  "l_out": 0,
+  "l_D": 65,
+  "m_D": 4332,
   "n": 1024,
   "s_D": 6,
-  "s_max": 8
+  "s_max": 128
 }
