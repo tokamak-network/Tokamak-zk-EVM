09b09502f692d046dea328cd8ae0be1c130e0039
Jehyuk Jang <jehyuk@Jehyukui-MacBookPro.local>
2025-08-14 21:22:46 +0800

    Restored Synthesizer from unintentional modification in the last commit.

--- packages/frontend/synthesizer/examples/demo/index.ts
+++ packages/frontend/synthesizer/examples/demo/index.ts
@@ -2,38 +2,30 @@
  * Run this file with:
  * DEBUG=ethjs,evm:*,evm:*:* tsx tansfer.ts
  */
-
 import { Address, hexToBytes } from '@synthesizer-libs/util';
 import { ethers } from 'ethers';
 import { createEVM } from '../../src/constructors.js';
 import { Finalizer } from '../../src/tokamak/core';
 import { getBlockHeaderFromRPC } from '../../src/tokamak/utils/index.js';
 import dotenv from 'dotenv';
-
 dotenv.config({
   path: '../../.env',
 });
 
-// const TRANSACTION_HASH =
-//   '0x2fc67302edd645958b58f22dc77013fac44ff51235f3fc16b64e51f561d701c8';
+const TRANSACTION_HASH =
+  '0x2fc67302edd645958b58f22dc77013fac44ff51235f3fc16b64e51f561d701c8';
 const RPC_URL = process.env.RPC_URL;
 
-const main = async (QAP_PATH: string, TRANSACTION_HASH: string) => {
-  if (!QAP_PATH) throw new Error('QAP_PATH is required');
-  if (!TRANSACTION_HASH) throw new Error('TRANSACTION_HASH is required');
-
+const main = async () => {
   if (!RPC_URL) {
     throw new Error('RPC_URL is not set');
   }
-
   const evm = await createEVM({
     txHash: TRANSACTION_HASH,
     rpcUrl: RPC_URL,
   });
-
   const provider = new ethers.JsonRpcProvider(RPC_URL);
   const tx = await provider.getTransaction(TRANSACTION_HASH);
-
   if (tx === null || tx.blockNumber === null) {
     throw new Error('Transaction not found or not yet mined');
   }
@@ -46,14 +38,11 @@ const main = async (QAP_PATH: string, TRANSACTION_HASH: string) => {
   if (tx.data === null) {
     throw new Error('Transaction data is null');
   }
-
   const { blockNumber, from, to, data, value, gasLimit } = tx;
-
   const actualTargetBlockHeader = await getBlockHeaderFromRPC(
     RPC_URL,
     blockNumber,
   );
-
   const evmInput = {
     to: new Address(hexToBytes(to)),
     caller: new Address(hexToBytes(from)),
@@ -76,15 +65,13 @@ const main = async (QAP_PATH: string, TRANSACTION_HASH: string) => {
   };
   // Now run the transfer
   const result = await evm.runCall(evmInput);
-
   // console.log('result.execResult : ', result.execResult);
-
   if (result.execResult.runState === undefined) {
     throw new Error('No synthesizer found');
   }
 
-  const finalizer = new Finalizer(QAP_PATH, result.execResult.runState.synthesizer.state);
+  const finalizer = new Finalizer(result.execResult.runState.synthesizer.state);
   const permutation = await finalizer.exec(undefined, true);
 };
 
-void main(process.argv[2], process.argv[3]).catch(console.error);
+void main().catch(console.error);
