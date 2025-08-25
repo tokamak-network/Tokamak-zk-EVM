881162ca67cc98a82e972e9cdc886620d15d5389
SonYoungsung <sysgigigi@gmail.com>
2025-08-25 12:47:00 +0900

    fix(synthesizer): path issue on runtime

--- packages/frontend/synthesizer/src/tokamak/constant/qap-static.ts
+++ packages/frontend/synthesizer/src/tokamak/constant/qap-static.ts
@@ -1,6 +1,7 @@
 // Static imports for QAP compiler files to fix binary compilation
 // This replaces the dynamic imports that don't work in compiled binaries
 
+import path from 'path';
 import { subcircuits as _subcircuits } from '../../../qap-compiler/subcircuits/library/subcircuitInfo.js';
 import { globalWireList as _globalWireList } from '../../../qap-compiler/subcircuits/library/globalWireList.js';
 import { setupParams as _setupParams } from '../../../qap-compiler/subcircuits/library/setupParams.js';
@@ -11,8 +12,6 @@ export const setupParams = _setupParams;
 
 // WASM directory path (dynamic based on execution environment)
 function getWasmDir(): string {
-  const path = require('path');
-
   // Check if running as Bun binary
   if ((process as any).isBun && process.execPath) {
     // Running as binary - use absolute path relative to binary location
