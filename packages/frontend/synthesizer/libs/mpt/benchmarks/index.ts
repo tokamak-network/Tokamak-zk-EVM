647f08926c2b96b6fec27103f8263941828febd1
Jehyuk Jang <jehyuk@Jehyukui-MacBookPro.local>
2025-11-06 20:19:20 +0800

    Deleted 'lib' folder

--- packages/frontend/synthesizer/libs/mpt/benchmarks/index.ts
+++
@@ -1,6 +0,0 @@
-import { createSuite } from './suite'
-import { LevelDB } from './engines/level'
-import { MapDB } from '@synthesizer-libs/util'
-
-createSuite(new MapDB())
-createSuite(new LevelDB())
