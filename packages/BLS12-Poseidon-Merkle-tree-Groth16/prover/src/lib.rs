a61697411ed54bf345f0efb10b0bbd9ed63309f6
mehdi-defiesta <mberiane@defiesta.net>
2025-11-07 10:33:32 +0100

    redesign the circuit by using tools

--- packages/BLS12-Poseidon-Merkle-tree-Groth16/prover/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod witness;
-pub mod proof;
-pub mod errors;
-pub mod poseidon;
-
-pub use witness::*;
-pub use proof::*;
-pub use errors::*;
-pub use poseidon::*;
--- packages/BLS12-Poseidon-Merkle-tree-Groth16/verifier/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod verifier;
-pub mod arkworks_verifier;
-pub mod errors;
-
-pub use verifier::*;
-pub use arkworks_verifier::*;
-pub use errors::*;
