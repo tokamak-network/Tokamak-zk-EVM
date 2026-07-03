9b1009ec84db2ed81fc23083ae4eb7e59bd0745d
Jehyuk <cjhyuck213@gmail.com>
2026-07-03 11:58:30 +0800

    Remove redundant verifier wrappers

--- packages/backend-wasm/src/verifier/index.ts
+++ packages/backend-wasm/src/verifier/index.ts
@@ -2,5 +2,4 @@ export * from "./binary-input.js";
 export * from "./challenges.js";
 export * from "./domain-context.js";
 export * from "./equations.js";
-export * from "./pairings.js";
 export * from "./verify-snark.js";
